4b0403f1c544e55f2427bb16b33c1927b78a74f0
Gennadya <heavyyrain3039@gmail.com>
2020-10-12 02:03:16 +0300

    препроцесс sass

--- webpack.config.js
+++ webpack.config.js
@@ -78,6 +78,20 @@ module.exports = {
                     }, 'css-loader']
             },
             {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        }
+                    }, 
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
                 test: /\.(png|jpg|svg|gif)$/,
                 use: ['file-loader']
             },
